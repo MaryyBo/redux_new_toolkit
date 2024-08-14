65fc4ce463c0242773569e8db2a97d1d7318266c
MaryyBo <marusya.bodrug@gmail.com>
2024-08-14 15:47:59 +0200

    Add themeSlice

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,14 @@
 const CONSTANTS = {
-LANGUAGE: {
-    EN_US: 'EN_US',
-    UA_UA: 'UA_UA',
-    PL_PL: 'PL_PL',
-    DE_DE: 'DE_DE'
-}
+    LANGUAGE: {
+        EN_US: 'EN_US',
+        UA_UA: 'UA_UA',
+        PL_PL: 'PL_PL',
+        DE_DE: 'DE_DE'
+    },
+    THEMES: {
+        DARK: 'DARK',
+        LIGHT: 'LIGHT'
+    }
 }
 
 export default CONSTANTS;
