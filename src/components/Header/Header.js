0f94b7667567bad22aa296282b6c61d13568f76a
MaryyBo <marusya.bodrug@gmail.com>
2024-08-14 14:32:40 +0200

    Add styles for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import styles from './Header.module.scss';
 
 const Header = () => {
     return (
-        <header>
+        <header className={styles.header}>
             <h1>My counter</h1>
             <nav>
-                <ul>
-                    <li>Link1</li>
-                    <li>Link2</li>
-                    <li>Link3</li>
-                    <li>Link4</li>
+                <ul className={styles.navList}>
+                    <li className={styles.navListItem}>Link1</li>
+                    <li className={styles.navListItem}>Link2</li>
+                    <li className={styles.navListItem}>Link3</li>
+                    <li className={styles.navListItem}>Link4</li>
                 </ul>
             </nav>
             <div>
